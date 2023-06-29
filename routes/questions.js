eb4b3355628879056736771780f742166dc840ab
unknown <sharmadharmesh1998@gmail.com>
2023-06-30 00:39:21 +0530

    questions routes are added

--- routes/questions.js
+++ routes/questions.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 // const userModel = require('../models/userModel');
 const jobModel = require('../models/JobsModel');
+// <--- Middleware --->
+
+const {checkLoggedIn, checkAdmin} = require('../middlewares/authUsers');
 
 
 // <---- add questions ----->
 
-router.get('/jobs/questions/add/:jobId', async(req, res)=>{
+router.get('/jobs/questions/add/:jobId', checkLoggedIn, checkAdmin, async(req, res)=>{
     try {
         const jobData = await jobModel.findById(req.params.jobId);
         res.render('../views/questions/new.ejs', {jobData});
@@ -15,7 +18,7 @@ router.get('/jobs/questions/add/:jobId', async(req, res)=>{
     }
 })
 
-router.post('/jobs/questions/add/:jobId', async(req, res)=>{
+router.post('/jobs/questions/add/:jobId',checkLoggedIn, checkAdmin,  async(req, res)=>{
     try {
         const questionData = {
             title: req.body.title,
@@ -39,7 +42,7 @@ router.post('/jobs/questions/add/:jobId', async(req, res)=>{
 
 // <---- View questions ----->
 
-router.get('/jobs/questions/view/:jobId', async(req, res)=>{
+router.get('/jobs/questions/view/:jobId',checkLoggedIn, checkAdmin,  async(req, res)=>{
     try {
         const jobData = await jobModel.findById(req.params.jobId);
         const questions = jobData.questions;
@@ -52,7 +55,7 @@ router.get('/jobs/questions/view/:jobId', async(req, res)=>{
 
 // <---- Edit questions ----->
 
-router.get('/jobs/questions/edit/:jobId/:quesId', async(req, res)=>{
+router.get('/jobs/questions/edit/:jobId/:quesId',checkLoggedIn, checkAdmin,  async(req, res)=>{
     try {
         const jobData = await jobModel.findById(req.params.jobId);
         const question = jobData.questions[req.params.quesId];
@@ -65,10 +68,10 @@ router.get('/jobs/questions/edit/:jobId/:quesId', async(req, res)=>{
 })
 
 
-router.patch('/jobs/questions/edit/:jobId/:quesId', async(req, res)=>{
+router.patch('/jobs/questions/edit/:jobId/:quesId',checkLoggedIn, checkAdmin,  async(req, res)=>{
     try {
         const job = await jobModel.findById(req.params.jobId);
-
+        
         const questionData = {
             title: req.body.title,
             option1: req.body.option1,
@@ -77,21 +80,35 @@ router.patch('/jobs/questions/edit/:jobId/:quesId', async(req, res)=>{
             option4: req.body.option4,
             ans: req.body.ans,
         };
-
+        
         job.questions[req.params.quesId] = questionData;
         await job.save();
+        req.flash('success', 'question has successfully updated');
         res.redirect(`/jobs/questions/view/${req.params.jobId}`);
-
+        
     } catch (error) {
+        req.flash('error', 'something went wrong while updating the question');
         console.log(error);
         res.redirect('/jobs');
     }
 })
 
 
+// <---- Delete questions ----->
 
-
-
+router.get('/jobs/questions/delete/:jobId/:quesId',checkLoggedIn, checkAdmin, async(req, res)=>{
+    try {
+        const job = await jobModel.findById(req.params.jobId);
+        const questions = job.questions;
+        questions.splice(req.params.quesId, 1);
+        await job.save();
+        req.flash('success', 'Question deleted');
+        res.redirect(`/jobs/questions/view/${req.params.jobId}`);
+    } catch (error) {
+        req.flash('error', 'something went wrong while deleting the question.');
+        console.log(error);
+    }
+})
 
 
 
